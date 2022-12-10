29694f138199f2e594f3662467ce35a6d093e447
arslonga83 <jlwood83@gmail.com>
2022-12-09 21:55:20 -0800

    update text in about section

--- about.js
+++ about.js
@@ -12,8 +12,8 @@ export const aboutHtml = `
   <p>After more than 20 years as a performing musician, I rediscovered a love of tech when I found myself working from home during the pandemic and learning the skills needed to record, edit, and produce weekly virtual music videos for my job. </p>
   <p>I have a life-long love of learning (some would describe me as a serial hobbyist) and I'm excited to embark on a new career path where I can keep learning new things in a contantly changing field.</p>
   <h4>What are my goals?</h4>
-  <p>I started simple and gradually grew my learning journal site. I would take notes about what I was learning. After each learning session, I'd use my notes to not only reflect on what I learned but also write short summaries of what I learned using my own words.</p>
-  <p>That helped me grok what I was learning, and I realized that posting my learning summaries was also helping others learn and stay motivated.</p>
+  <p>I want to keep learning new skills. I feel that I can reach a point where I'm qualified to apply for coding jobs.</p>
+  <p>I'm also realistic, and don't expect to be single-handedly coding the next Facebook in the next few months. I hope to find an entry-level position with a great team of colleagues who I can learn from as I begin my new career.</p>
 </div>
 <span class="posts-header">Recent Posts</span>
 </div>
